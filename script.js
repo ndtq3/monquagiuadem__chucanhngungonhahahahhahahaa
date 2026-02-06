bbabc2ca147fe5bbd6b18be0ae2fb06f97ec8e58
Thảo Quyên So Cute <quyenquyen2090@gmail.com>
2026-02-07 08:34:20 +1200

    Refactor show/hide functions and add hideAllText
    Refactor show and hide functions to check for element existence. Introduce hideAllText function to hide all text elements.

--- script.js
+++ script.js
@@ -1,76 +1,84 @@
+/* ===== TIỆN ÍCH ===== */
 const $ = id => document.getElementById(id);
-const show = id => $(id).style.opacity = 1;
-const hide = id => $(id).style.opacity = 0;
 
-const butterfly = $("butterfly");
+const show = id => {
+  if ($(id)) $(id).style.opacity = 1;
+};
 
-/* ẨN TẤT CẢ TEXT BAN ĐẦU */
-[
-  "text0","text1","text2","text3","text4","text5","text6",
-  "text7","text10","text11","text12","text13","text14"
-].forEach(hide);
+const hide = id => {
+  if ($(id)) $(id).style.opacity = 0;
+};
 
-/* GỢI Ý BAN ĐẦU */
-setTimeout(()=>show("text0"),1000);
+const hideAllText = () => {
+  document.querySelectorAll(".text").forEach(t => {
+    t.style.opacity = 0;
+  });
+};
 
-/* CLICK BƯỚM */
-butterfly.addEventListener("click", () => {
+/* ===== KHỞI TẠO ===== */
+hideAllText();
+setTimeout(() => show("text0"), 800);
 
-  /* BƯỚM BAY */
-  butterfly.style.top = "-30%";
-  butterfly.style.opacity = 0;
+/* ===== CLICK BƯỚM ===== */
+$("butterfly").onclick = () => {
 
-  /* ẢNH ÁO XANH */
-  show("imgT");
-  setTimeout(()=>hide("imgT"),2000);
+  // BƯỚM BAY
+  $("butterfly").style.top = "-30%";
+  $("butterfly").style.opacity = 0;
+  hide("text0");
 
-  /* TEXT CHÍNH */
-  let t = 2000;
-  ["text1","text2","text3","text4","text5","text6"].forEach(id=>{
-    setTimeout(()=>show(id),t);
-    setTimeout(()=>hide(id),t+1800);
-    t+=2000;
+  /* ===== TIMELINE TEXT ===== */
+  const texts = [
+    "text1",
+    "text2",
+    "text3",
+    "text4",
+    "text5",
+    "text6",
+    "text7",
+    "text10",
+    "text11",
+    "text12",
+    "text13",
+    "text14"
+  ];
+
+  let t = 1500;
+
+  texts.forEach(id => {
+    setTimeout(() => {
+      hideAllText();
+      show(id);
+    }, t);
+    t += 2000;
   });
 
-  /* ÁO ĐEN */
-  setTimeout(()=>{
-    show("text2");
-    show("imgH");
-  },6000);
+  /* ===== TIMELINE ẢNH ===== */
 
-  setTimeout(()=>hide("imgH"),9000);
+  // ÁO XANH
+  setTimeout(() => show("imgT"), 1500);
+  setTimeout(() => hide("imgT"), 3500);
 
-  /* CÔNG VIỆC */
-  setTimeout(()=>show("imgT1"),10000);
-  setTimeout(()=>show("imgT2"),10500);
-  setTimeout(()=>show("imgQ"),12500);
+  // ÁO ĐEN
+  setTimeout(() => show("imgH"), 3500);
+  setTimeout(() => hide("imgH"), 6500);
 
-  /* KHÓC */
-  setTimeout(()=>{
-    show("text7");
-    show("imgN");
-  },15000);
+  // CÔNG VIỆC
+  setTimeout(() => show("imgT1"), 6500);
+  setTimeout(() => show("imgT2"), 7000);
 
-  setTimeout(()=>{
-    hide("imgN");
-    hide("text7");
-  },17500);
+  // LÈ LƯỠI
+  setTimeout(() => show("imgQ"), 9000);
 
-  /* TRÁI TIM + ĐỘNG VIÊN */
-  setTimeout(()=>{
-    startGalaxy();
-    startHearts();
-    show("text10");
-    show("imgH1");
-  },18000);
+  // KHÓC
+  setTimeout(() => show("imgN"), 11500);
+  setTimeout(() => hide("imgN"), 14000);
 
-  setTimeout(()=>{
-    hide("imgH1");
-    show("text11");
-    show("imgH2");
-  },21000);
+  // CUỐI
+  setTimeout(() => show("imgH1"), 15000);
+  setTimeout(() => show("imgH2"), 17500);
 
-  setTimeout(()=>show("text12"),23000);
-  setTimeout(()=>show("text13"),24500);
-  setTimeout(()=>show("text14"),27000);
-});
+  /* ===== HIỆU ỨNG ===== */
+  startGalaxy();
+  setTimeout(startHearts, 14000);
+};
